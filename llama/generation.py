8ab9f48e2cbafa85c9dadc47f774a0e77a88e585
Liyang Lu <liyanglu@google.com>
2023-05-19 21:14:30 +0000

    add comment

--- llama/generation.py
+++ llama/generation.py
@@ -72,6 +72,8 @@ class LLaMA:
         tokens = tokens.to(device)
         input_text_mask = tokens != self.tokenizer.pad_id
 
+        # Passing tensors instead of floats into self._generate_one_token_fn,
+        # so that different values would not trigger compilations of new graphs
         temperature_tensor = torch.tensor(temperature).to(device)
         top_p_tensor = torch.tensor(top_p).to(device)
         with_temp = temperature > 0
