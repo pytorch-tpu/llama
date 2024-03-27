5dc0a2b704152019a3a20d47ab73591e10ae5e4b
Yeounoh Chung <yeounoh@google.com>
2024-03-27 21:14:59 +0000

    Testing

--- llama/generation.py
+++ llama/generation.py
@@ -139,28 +139,30 @@ class Llama:
             num_devices = self.num_devices  # Should be 8 on v5-8
             device_ids = np.arange(num_devices)
 
-            mesh = xs.Mesh(device_ids, (num_devices,))
+            mesh = xs.Mesh(device_ids, (num_devices, 1), axis_names=('data', 'model'))
+            row_partition = ('data', 'model')
+            col_partition = ('model', 'data')
 
             # manually shard the kv cache
-            for layer in model.layers:
-                xs.mark_sharding(layer.attention.cache_k, mesh, (None, None, 0, None))
-                xs.mark_sharding(layer.attention.cache_v, mesh, (None, None, 0, None))
+            # for layer in model.layers:
+            #    xs.mark_sharding(layer.attention.cache_k, mesh, (None, None, 0, None))
+            #    xs.mark_sharding(layer.attention.cache_v, mesh, (None, None, 0, None))
 
             for name, layer in model.named_modules():
                 if 'tok_embeddings' in name:
-                    xs.mark_sharding(layer.weight, mesh, (0, None))
+                    xs.mark_sharding(layer.weight, mesh, row_partition)
                 if 'attention.' in name:
                     if 'wo' in name:
-                        xs.mark_sharding(layer.weight, mesh, (0, None))
+                        xs.mark_sharding(layer.weight, mesh, row_partition)
                     else:
-                        xs.mark_sharding(layer.weight, mesh, (None, 0))
+                        xs.mark_sharding(layer.weight, mesh, col_partition)
                 if 'feed_forward.' in name:
                     if 'w2' in name:
-                        xs.mark_sharding(layer.weight, mesh, (0, None))
+                        xs.mark_sharding(layer.weight, mesh, row_partition)
                     else:
-                        xs.mark_sharding(layer.weight, mesh, (None, 0))
+                        xs.mark_sharding(layer.weight, mesh, col_partition)
                 if 'output' in name:
-                    xs.mark_sharding(layer.weight, mesh, (None, 0))
+                    xs.mark_sharding(layer.weight, mesh, col_partition)
 
         if dynamo:
             if USE_CUDA:
